0188fc546dd2e63582341fddec639ac9f8a6b532
BOURDON Marin <104168812+shaz267@users.noreply.github.com>
2022-05-26 21:52:14 +0100

    Create HugeFloat.c

---
+++ HugeFloat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "HugeFloat.h"
+
+HugeFloat* createHugeFloat (void) {
+    return NULL;
+}
+
+HugeFloat* createHugeFloatFromString (const char* significandString, const char* exponentString) {
+    return NULL;
+}
+
+HugeFloat* createHugeFloatFromHugeInts (const HugeInt* significandHugeInt, const HugeInt* exponentSHugeInt) {
+    return NULL;
+}
+
+HugeFloat* createHugeFloatFromHugeFloat (const HugeFloat* hugeFloat, const unsigned int size) {
+    return NULL;
+}
+
+void deleteHugeFloat (const HugeFloat* hugeFloat) {
+}
+
+int getHugeFloatLength (const HugeFloat* hugeFloat) {
+    return 0;
+}
+
+void printHugeFloat (const HugeFloat* hugeFloat) {
+    char* hugeFloatString = HugeFloatToString (hugeFloat);
+    printf ("%s\n", hugeFloatString);
+    free (hugeFloatString);
+}
